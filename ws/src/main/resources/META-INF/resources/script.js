ca379e31f6a65aca107aa27ff6ce9033902ddc6d
vlad <evl.evl@gmail.com>
2021-11-09 21:48:38 +0100

    js fixes

--- ws/src/main/resources/META-INF/resources/script.js
+++ ws/src/main/resources/META-INF/resources/script.js
@@ -3,11 +3,8 @@ const app = Vue.createApp({
   data() {
     return {
       source: [
-        { text: "Quelque", t:'e', whitespace: " " },
-        { text: "chose", whitespace: " " },
-        { text: "roule", whitespace: " " },
-        { text: "vite", t:'w', whitespace: "" },
-        { text: ".", whitespace: "" }
+        { text: "Le fautes d'orthographe et les erreurs grammaticales peuvent affecter votre crédibilité.", whitespace: " " },
+        { text: "Il en va de même pour les virgules mal utilisées et les autres types de ponctuation.", whitespace: "" },
       ],
       todos: []
     };
@@ -21,50 +18,40 @@ const app = Vue.createApp({
     sourceText() {
       return this.source.map((x) => x.text + x.whitespace).join("");
     },
-    filteredTodos() {
+    suggestions() {
       return this.todos;
     }
   },
   methods: {
-    update: _.debounce(function (e) {
-      this.fetchData(e.target.innerText);
-    }, 300),
+    onEdit: _.debounce(function (e) {
+      this.fetchSuggestions();
+    }, 1000),
     applyTodo(todo) {
       if (!todo.disabled) {
         todo.disabled = true;
-        this.source.splice(todo.s, todo.l, ...todo.w);
-        this.adjustTodos(todo);
+        for (var i = 0; i < todo.replacements.length; i++) {
+          var r = todo.replacements[i];
+          this.source.splice(r.startIdx, r.len, ...r.replacement);
+        }
+        // do I need to adapt existing todos?
         this.finishUpdatesLater(todo);
       }
     },
-    adjustTodos(todo) {
-      var delta = todo.w.length - todo.l;
-      for (var i = 0; i < this.todos.length; i++) {
-        if (this.todos[i].s > todo.s) {
-          this.todos[i].s += delta;
-        }
-      }
-    },
     finishUpdatesLater: _.debounce(function (todo) {
-      this.removeTodo(todo);
-      this.fetchData(this.sourceText)
-    }, 500),
-    removeTodo(todo) {
       this.todos.splice(this.todos.indexOf(todo), 1);
-    },
-    fetchData(text) {
+      this.fetchSuggestions();
+    }, 500),
+    fetchSuggestions() {
       var xhr = new XMLHttpRequest();
       var self = this;
-      xhr.open("GET", apiURL + text);
+      xhr.open("GET", apiURL + this.sourceText);
       xhr.onload = function() {
         var res = JSON.parse(xhr.responseText);
         self.source = res.sentence;
         self.todos = res.suggestions.map((x, idx) => ({
             id: idx,
             title: x.title,
-            s: x.replacements[0].startIdx,
-            l: x.replacements[0].len,
-            w: x.replacements[0].replacement,
+            replacements: x.replacements,
             t: 'e'
         }));
       };
